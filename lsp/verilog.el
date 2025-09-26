8539079431880e9fceb6ba16cf97e44dd415fb62
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2025-09-26 16:56:30 +0200

    Update default config

--- lsp/verilog.el
+++ lsp/verilog.el
@@ -16,19 +16,12 @@
   (dolist (pattern '("\\.v\\'" "\\.sv\\'" "\\.svh\\'"))
     (add-to-list 'auto-mode-alist (cons pattern 'verilog-ts-mode))))
 
-;; (with-eval-after-load 'eglot
-;;   (add-to-list 'eglot-server-programs
-;;                `((verilog-mode)
-;;                  . ,(list "circt-verilog-lsp-server"
-;;                           "--command-file"
-;;                           (expand-file-name "/Users/scheremo/devel/snitch_upstream/compile_arcilator.f")))))
-
 
 (use-package verilog-eglot-bender
   :load-path "~/.emacs.d/veb"
   :hook (verilog-ts-mode . veb/on-verilog-buffer)
   :custom
-  (veb-filelist-name ".circt-verilog.f")
+  (veb-filelist-name "target/slang/build/slang-flist-simulation.f")
   (veb-lsp-executable "circt-verilog-lsp-server")
   (veb-debug 0)
   )
